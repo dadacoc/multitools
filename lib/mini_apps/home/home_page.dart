5bf9a8e1b0608d2f54d0d2b07d7a279cac5ad13b
dadacoc <davidlequere39@gmail.com>
2025-06-29 17:34:38 +0200

    feat: Add navigation bar and logic with

--- lib/mini_apps/home/home_page.dart
+++ lib/mini_apps/home/home_page.dart
@@ -1,8 +1,49 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  //Navigation
+  final int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    switch (index){
+      case 0:
+        context.go('/');
+        break;
+
+      case 1:
+        break;
+    }
+  }
+
+  late Timer _timer;
+  late DateTime _currentTime;
+
+  @override
+  void initState() {
+    super.initState();
+    _currentTime = DateTime.now();
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer){
+      setState(() {
+        _currentTime = DateTime.now();
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,27 +59,36 @@ class HomePage extends StatelessWidget {
             Image.asset("assets/images/BoiteAOutils.jpg"),
             const Text("Bienvenue sur Multitools !",
               style: TextStyle(
-                fontSize: 24
+                  fontSize: 24
               ),
             ),
             Container(
               margin: const EdgeInsets.only(top: 10),
               child: ElevatedButton.icon(
-                  onPressed: (){
-                    context.go('/Calculatrice');
-                  },
-                  label: const Text("Entrer dans l'application !",
+                onPressed: (){
+                  context.go('/Calculatrice');
+                },
+                label: const Text("Entrer dans l'application !",
                   style: TextStyle(
-                    fontSize: 24,
-                    color: Colors.green
+                      fontSize: 24,
+                      color: Colors.green
                   ),
-                  ),
-                  icon: const Icon(Icons.launch , size: 24,color: Colors.green,),
+                ),
+                icon: const Icon(Icons.launch , size: 24,color: Colors.green,),
               ),
             )
           ],
         ),
       ),
+
+      bottomNavigationBar: NavigationBar(
+          destinations: [
+            NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
+            NavigationDestination(icon: Icon(Icons.apps), label: 'Catalogue')
+          ],
+        selectedIndex: _selectedIndex,
+        onDestinationSelected: _onItemTapped ,
+      )
     );
   }
 }
