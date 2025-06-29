6c3fc3a5a1e007f85177229241b5ac4780c9065e
dadacoc <davidlequere39@gmail.com>
2025-06-29 17:36:33 +0200

    chore: Add the intl dependency

--- lib/mini_apps/home/home_page.dart
+++ lib/mini_apps/home/home_page.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:intl/intl.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
